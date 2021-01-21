4d42b167553f2a42cccfe553a1e01c67aea5793a
bnthp16 <77015847+bnthp16@users.noreply.github.com>
2021-01-22 00:35:41 +0100

    Update crypto.rb

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,34 +1,29 @@
 require 'open-uri'
 require 'nokogiri'
 
+market = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
 
-def darktrader
-	
-	doc = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
-	arr_b = []
-	arr_v = []
-
-	bitcoin = doc.xpath("//tr/td[3]")
-	value = doc.xpath("//tr/td[4]")
-
+def symbol (market)
+	arr_s = []
+	bitcoin = market.xpath("//tr/td[3]")
 	bitcoin.each do |name| 
-		arr_b << name.text
+		arr_s << name.text
 	end
+	return arr_s
+end
 
+def values (market)
+	arr_v = []
+	value = market.xpath("//tr/td[4]")
 	value.each do |v|
 		arr_v << v.text.tr('$', '')
 	end
-
-	r = Hash[arr_b.zip(arr_v)]
-
-	print r
+	return arr_v
 end
 
-darktrader
+def all (market)
+	arr_all = symbol(market).zip(values(market)).map{|x|[ x].to_h}
+	print arr_all
+end
 
-
-
-
-
-	
-	  
+all(market)
