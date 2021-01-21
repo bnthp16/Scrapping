e7d37d71ca0eaa4f9784be48c73248952033020b
bnthp16 <77015847+bnthp16@users.noreply.github.com>
2021-01-21 23:58:51 +0100

    Update emails.rb

--- lib/emails.rb
+++ lib/emails.rb
@@ -1,43 +1,29 @@
 require 'open-uri'
 require 'nokogiri'
 
-# def get_townhall_email
-# doc = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/95/avernes.html"))
-#     doc.search('//td').each do |mail|
-#         email = mail.content
-#         print email.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i)
-#     end
+market = Nokogiri::HTML(URI.open('https://coinmarketcap.com/all/views/all/'))
 
-# end
-# get_townhall_email
-
-valdoise = Nokogiri::HTML(URI.open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
-
-def get_townhall_urls(valdoise) 
-
-    arr_url = [] #récupère les liens des mairies
-    villes_val = valdoise.search('.lientxt').map do |nom|
-        arr_url << "https://www.annuaire-des-mairies.com/95/#{nom.text.downcase.tr(' ', '-')}.html"
-    end
-    return arr_url
+def symbol (market)
+	arr_s = []
+	bitcoin = market.xpath("//tr/td[3]")
+	bitcoin.each do |name| 
+		arr_s << name.text
+	end
+	return arr_s
 end
 
-def get_townhall_email(valdoise) #récupère les email
-    arr_e = []
-    get_townhall_urls(valdoise).map do |url|
-    arr_e << Nokogiri::HTML(URI.open(url)).search('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-    end
-    return arr_e
+def values (market)
+	arr_v = []
+	value = market.xpath("//tr/td[4]")
+	value.each do |v|
+		arr_v << v.text.tr('$', '')
+	end
+	return arr_v
 end
 
-def valdoise_arr(valdoise) #rassemble les deux array en hash
-    hash_names_emails = get_townhall_urls(valdoise).zip(get_townhall_email(valdoise)).map{|x| [x].to_h}
-    puts hash_names_emails
+def all (market)
+	arr_all = values(market.zip)(symbol(market)).map{|x| x.to_h}
+	puts arr_all
 end
 
-valdoise_arr(valdoise)
-
-
-
-
-
+all
